1775fc78e6021696a9fd47fdac37b4053797fedf
Tatsiana13 <74614955+Tatsiana13@users.noreply.github.com>
2021-02-01 21:51:52 +0300

    Update table.php
    Add column "Time" and display variable.

--- table.php
+++ table.php
@@ -10,13 +10,13 @@
     <?php
 $data = file_get_contents("guest_data.txt");
 $records = explode("<======>", $data);
-echo "<div class=table>";
+echo "<div class='table'>";
 echo "<table border=1>";
-echo "<th>Name</th><th>Email</th><th>Messag</th>";
+echo "<th>Name</th><th>Email</th><th>Messag</th><th>Time</th>";
 foreach ($records as $record) {
     $row = explode("\n", trim($record));
 
-    echo "<tr><td>$row[0]</td><td>$row[1]</td><td>$row[2]</td></tr>";
+    echo "<tr><td>$row[0]</td><td>$row[1]</td><td>$row[2]</td><td>$row[3]</td></tr>";
 }
 
 echo "</table>";
