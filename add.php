0cfca919a4914c3e09b0b3f7a9148b2784b31fcd
Tatsiana13 <74614955+Tatsiana13@users.noreply.github.com>
2021-02-01 21:46:51 +0300

    Update add.php
    Assign a value to variable.

--- add.php
+++ add.php
@@ -6,7 +6,8 @@ if ((!empty($_POST)) && ($_POST["name"] != "") && ($_POST["email"] != "") && ($_
         $_POST["name"] . "\n" .
         $_POST["email"] . "\n" .
         $_POST["text"] . "\n" .
-        $dateTime = date("d-m-Y H:i:s");
+        $_POST["ok"] = date("d-m-Y H:i:s");
+//         $dateTime = date("d-m-Y H:i:s");
     file_put_contents("guest_data.txt", $row, FILE_APPEND);
     header("Location: ?");
     die();  
